62ea407eb6621173775eabcb1898af6af6538a8c
Diego Cono <diegogabrielcono@gmail.com>
2025-06-02 21:31:31 -0300

    agregando el backend

---
+++ backend/lifting-arrangement/src/main/java/com/liftingarrangement/lifting_arrangement/LiftingArrangementApplication.java
@@ -0,0 +1,13 @@
+package com.liftingarrangement.lifting_arrangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftingArrangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftingArrangementApplication.class, args);
+	}
+
+}
---
+++ backend/lifting-arrangement/src/test/java/com/liftingarrangement/lifting_arrangement/LiftingArrangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liftingarrangement.lifting_arrangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftingArrangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
